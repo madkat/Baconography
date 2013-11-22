a1479085109a75a8e210657e56e53349704e517d
Jeff Greene <jeff.greene@synergex.com>
2013-11-21 22:07:53 -0800

    added helper

---
+++ SnooStreamWP8/View/Controls/LoadMoreControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls.Primitives;
+
+namespace SnooStreamWP8.View.Controls
+{
+    public class LoadMoreControl : RadContentControl
+    {
+        public LoadMoreControl()
+        {
+            this.DefaultStyleKey = typeof(LoadMoreControl);
+        }
+    }
+}
