fefd5f05908572b9483ba6f90ee032ce66c5e84e
hippiehunter <hippiehunterenator@gmail.com>
2014-01-15 21:27:42 -0800

    added converter for view model image source -> actual image source

---
+++ SnooStreamWP8/Converters/AsyncImageSourceConverter.cs
@@ -0,0 +1,35 @@
+﻿using SnooStream.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace SnooStreamWP8.Converters
+{
+    public class AsyncImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = new BitmapImage();
+            var imageSource = value as SnooStream.Common.ImageSource;
+            imageSource.ImageData.ContinueWith(tsk =>
+                {
+                    if (tsk.IsCompleted)
+                        result.SetSource(new MemoryStream(tsk.Result));
+                    else if(Uri.IsWellFormedUriString(imageSource.UrlSource, UriKind.Absolute))
+                        result.UriSource = new Uri(imageSource.UrlSource);
+                }, SnooStreamViewModel.UIScheduler);
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
