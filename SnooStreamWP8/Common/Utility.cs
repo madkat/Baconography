d69ef70cd956e64936835a0a9474a8ca644aae0e
hippiehunter <hippiehunterenator@gmail.com>
2014-03-19 08:26:28 -0700

    added missing classes

---
+++ SnooStreamWP8/Common/Utility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SnooStreamWP8.Common
+{
+    public class Utility
+    {
+        public static SolidColorBrush GetColorFromHexa(string hexaColor)
+        {
+            return new SolidColorBrush(
+                Color.FromArgb(
+                    Convert.ToByte(hexaColor.Substring(1, 2), 16),
+                    Convert.ToByte(hexaColor.Substring(3, 2), 16),
+                    Convert.ToByte(hexaColor.Substring(5, 2), 16),
+                    Convert.ToByte(hexaColor.Substring(7, 2), 16)
+                )
+            );
+        }
+
+        public const string ReadMailGlyph = "\uE166";
+        public const string UnreadMailGlyph = "\uE119";
+
+
+
+        private static bool loadingActiveLockScreen = false;
+
+        private static string CleanRedditLink(string userInput, string username)
+        {
+            if (userInput == "/")
+                return userInput;
+
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                var selfMulti = "/" + username + "/m/";
+                if (userInput.Contains(selfMulti))
+                {
+                    return "/me/m/" + userInput.Substring(userInput.IndexOf(selfMulti) + selfMulti.Length);
+                }
+            }
+
+            if (userInput.StartsWith("me/m/"))
+                return "/" + userInput;
+            else if (userInput.StartsWith("/m/"))
+                return "/me" + userInput;
+            else if (userInput.StartsWith("/me/m/"))
+                return userInput;
+
+            if (userInput.StartsWith("/u/"))
+            {
+                return userInput.Replace("/u/", "/user/");
+            }
+
+            if (userInput.StartsWith("r/"))
+                return "/" + userInput;
+            else if (userInput.StartsWith("/") && !userInput.StartsWith("/r/"))
+                return "/r" + userInput;
+            else if (userInput.StartsWith("/r/"))
+                return userInput;
+            else
+                return "/r/" + userInput;
+        }
+    }
+}
---
+++ SnooStreamWP8/Converters/AuthorFlairKindConverter.cs
@@ -0,0 +1,91 @@
+﻿using SnooSharp;
+using SnooStreamWP8.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SnooStreamWP8.Converters
+{
+    public class AuthorFlairKindConverter : IValueConverter
+    {
+        static SolidColorBrush bg_none = new SolidColorBrush(Colors.Transparent);
+        static SolidColorBrush bg_mod = new SolidColorBrush(Colors.Transparent);
+        static SolidColorBrush bg_op;
+
+        static AuthorFlairKindConverter()
+        {
+            if (Application.Current.Resources.Contains("PhoneAccentBrush"))
+                bg_op = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+            else
+                bg_op = Utility.GetColorFromHexa("#FFDAA520");
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var kind = (AuthorFlairKind)value;
+            switch (kind)
+            {
+                case AuthorFlairKind.OriginalPoster:
+                    return bg_op;
+                case AuthorFlairKind.Moderator:
+                    return bg_mod;
+                case AuthorFlairKind.None:
+                    return bg_none;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ForegroundAuthorFlairKindConverter : IValueConverter
+    {
+        static SolidColorBrush fg_none;
+        static SolidColorBrush fg_mod;
+        static SolidColorBrush fg_op;
+
+        static ForegroundAuthorFlairKindConverter()
+        {
+            if (Application.Current.Resources.Contains("PhoneAccentBrush"))
+                fg_none = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+            else
+                fg_none = Utility.GetColorFromHexa("#FFDAA520");
+
+            if (Application.Current.Resources.Contains("PhoneForegroundBrush"))
+                fg_op = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
+            else
+                fg_op = Utility.GetColorFromHexa("#FFDAA520");
+
+            fg_mod = new SolidColorBrush(Colors.Green);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var kind = (AuthorFlairKind)value;
+
+            switch (kind)
+            {
+                case AuthorFlairKind.OriginalPoster:
+                    return fg_op;
+                case AuthorFlairKind.Moderator:
+                    return fg_mod;
+                case AuthorFlairKind.None:
+                    return fg_none;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
